8a57fe9638746ffe0566e4371cddce0d812862a9
Rodrigo Castro <castro.rl@gmail.com>
2022-07-28 16:32:45 -0300

    starting the Card.js and Card.css

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <h1>Hello World</h1> */}
-    <App />
+    <Card />
   </React.StrictMode>
 );
 
